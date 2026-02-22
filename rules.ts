b37072b83fdb0f892b947045f1d52dbb59696be5
Yash Shrestha <yashcrest@gmail.com>
2026-02-22 16:17:31 +1100

    chore: removed commented line

--- rules.ts
+++ rules.ts
@@ -38,21 +38,6 @@ const rules: KarabinerRules[] = [
         ],
         type: "basic",
       },
-      //      {
-      //        type: "basic",
-      //        description: "Disable CMD + Tab to force Hyper Key usage",
-      //        from: {
-      //          key_code: "tab",
-      //          modifiers: {
-      //            mandatory: ["left_command"],
-      //          },
-      //        },
-      //        to: [
-      //          {
-      //            key_code: "tab",
-      //          },
-      //        ],
-      //      },
     ],
   },
   ...createHyperSubLayers({
