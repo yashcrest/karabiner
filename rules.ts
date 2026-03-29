11fe380e387c8c1607de638a7890ea7207a0fc35
Yash Shrestha <yash.shrestha@latitudefinancial.com>
2026-03-29 19:12:28 +1100

    fix: removed duplicate `i` on `s` sublayer

--- rules.ts
+++ rules.ts
@@ -100,7 +100,7 @@ const rules: KarabinerRules[] = [
     s: {
       // "D"o not disturb toggle
       d: open(
-        `raycast://extensions/yakitrak/do-not-disturb/toggle?launchType=background`,
+        `raycast://extensions/yakitrak/do-not-disturb/toggle?launchType=background`
       ),
       u: {
         to: [
@@ -133,7 +133,6 @@ const rules: KarabinerRules[] = [
           },
         ],
       },
-	i: open(`-b com.apple.SystemProfile`),
 
       semicolon: {
         to: [
@@ -199,11 +198,11 @@ const rules: KarabinerRules[] = [
     r: {
       //   clip'b'oard
       b: open(
-        "raycast://extensions/raycast/clipboard-history/clipboard-history",
+        "raycast://extensions/raycast/clipboard-history/clipboard-history"
       ),
       // emo'j'i
       j: open(
-        "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols",
+        "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
       ),
       //   c: open("raycast://extensions/thomas/color-picker/pick-color"),
       //   n: open("raycast://script-commands/dismiss-notifications"),
@@ -240,6 +239,6 @@ fs.writeFileSync(
       ],
     },
     null,
-    2,
-  ),
+    2
+  )
 );
